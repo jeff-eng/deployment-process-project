b2b89982b48f74d2e0f646d5156fc7f7772b8eb4
JEFF ENG <ME@JEFFENG.COM>
2022-09-20 06:57:02 -0700

    Modified the deploy.sh file to include AWS access key credentials.

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -2,7 +2,6 @@ eb list
 
 eb use udagram-api-dev
 
-eb setenv NAME=$NAME
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 eb setenv POSTGRES_HOST=$POSTGRES_HOST
